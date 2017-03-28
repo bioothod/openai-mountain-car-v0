7af8446f97f839426a51a6d735d9574b68c0c999
Evgeniy Polyakov <zbr@ioremap.net>
2017-03-28 17:35:32 +0300

    nn: save exported params into dictionary and do not try to find it every time export_params() is called, this allows to export params from multiple threads (tf.variable_scope() modifies graph, which is not threadsafe). Get rid of self.merged operation, which included all summaries including the ones from previous nn class initializations, handle list of summaries by hand

--- nn.py
+++ nn.py
@@ -15,7 +15,8 @@ class nn(object):
         print "going to initialize scope %s" % scope
         self.summary_writer = summary_writer
         self.scope = scope
-        with tf.variable_scope(scope):
+        with tf.variable_scope(scope) as vscope:
+            self.vscope = vscope
             self.do_init(input_size, output_size)
             print "scope %s has been initialized" % scope
 
@@ -33,6 +34,9 @@ class nn(object):
         sb = tf.summary.histogram(bname, b)
         self.summary_weights.append(sb)
 
+        self.transform_params[wname] = w
+        self.transform_params[bname] = b
+
         wext = tf.placeholder(tf.float32, dims, name=wname+'_ext')
         bext = tf.placeholder(tf.float32, [dims[1]], name=bname+'_ext')
 
@@ -42,14 +46,13 @@ class nn(object):
         b_transform_ops = b.assign(b * (1 - self.transform_lr) + bext * self.transform_lr)
         self.transform_ops.append(b_transform_ops)
 
-        self.transform_params.append(wname)
-        self.transform_params.append(bname)
-
         return w, b
 
     def init_model(self, input_size, output_size):
-        layers = [50, 190, output_size]
-        #layers = [256, output_size]
+        #layers = [50, 190, output_size]
+        layers = [256, output_size]
+
+        print "init_model scope: %s" % (tf.get_variable_scope().name)
 
         x = tf.placeholder(tf.float32, [None, input_size], name='x')
         y = tf.placeholder(tf.float32, [None, output_size], name='y')
@@ -65,17 +68,17 @@ class nn(object):
             h = tf.add(tf.matmul(input_layer, w), b, name=hname)
 
             if idx == len(layers) - 1:
-                tf.summary.histogram('model', h)
+                self.add_summary(tf.summary.histogram('model', h))
 
                 mse = tf.reduce_mean(tf.square(h - y))
                 reg_loss = tf.add_n(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
                 loss = tf.add(mse, reg_loss, name='loss')
-                tf.summary.scalar('mse', mse)
-                tf.summary.scalar('loss', loss)
+                self.add_summary(tf.summary.scalar('mse', mse))
+                self.add_summary(tf.summary.scalar('loss', loss))
                 return h, loss
             else:
                 nl_h = tf.nn.tanh(h, name='nonlinear_' + hname)
-                tf.summary.histogram('nonlinear_' + hname, nl_h)
+                self.add_summary(tf.summary.histogram('nonlinear_' + hname, nl_h))
                 input_layer = nl_h
 
             input_dimension = l
@@ -84,6 +87,8 @@ class nn(object):
 
         return None, None
 
+    def add_summary(self, s):
+        self.summary_all.append(s)
 
     def do_init(self, input_size, output_size):
         self.learning_rate_start = 0.0025
@@ -93,8 +98,9 @@ class nn(object):
         self.train_num = 0
 
         self.transform_ops = []
-        self.transform_params = []
+        self.transform_params = {}
 
+        self.summary_all = []
         self.summary_weights = []
         self.episode_stats_update = []
 
@@ -103,10 +109,10 @@ class nn(object):
         self.learning_rate = 0.00001 + tf.train.exponential_decay(self.learning_rate_start, global_step, 30000, 0.6, staircase=True)
         self.reg_beta = tf.train.exponential_decay(self.reg_beta_start, global_step, 30000, 0.6, staircase=True)
 
-        tf.summary.scalar('reg_beta', self.reg_beta)
-        tf.summary.scalar('transform_lr', self.transform_lr)
-        tf.summary.scalar('learning_rate', self.learning_rate)
-        tf.summary.scalar('global_step', global_step)
+        self.add_summary(tf.summary.scalar('reg_beta', self.reg_beta))
+        self.add_summary(tf.summary.scalar('transform_lr', self.transform_lr))
+        self.add_summary(tf.summary.scalar('learning_rate', self.learning_rate))
+        self.add_summary(tf.summary.scalar('global_step', global_step))
 
         episodes_passed_p = tf.placeholder(tf.int32, [], name='episodes_passed')
         episode_reward_p = tf.placeholder(tf.float32, [], name='episode_reward')
@@ -117,8 +123,8 @@ class nn(object):
         self.episode_reward = tf.get_variable('episode_reward', [], initializer=tf.constant_initializer(0), trainable=False)
         self.episode_stats_update.append(self.episode_reward.assign(episode_reward_p))
 
-        tf.summary.scalar('episodes_passed', self.episodes_passed)
-        tf.summary.scalar('episode_reward', self.episode_reward)
+        self.add_summary(tf.summary.scalar('episodes_passed', self.episodes_passed))
+        self.add_summary(tf.summary.scalar('episode_reward', self.episode_reward))
 
         self.model, self.loss = self.init_model(input_size, output_size)
 
@@ -129,16 +135,18 @@ class nn(object):
 
         self.optimizer_step = opt.minimize(self.loss, global_step=global_step)
         grads = opt.compute_gradients(self.loss)
-        self.compute_gradients_step = [grad[0] for grad in grads]
-
+        self.compute_gradients_step = []
         self.gradient_names = []
         apply_grads = []
         for e in grads:
+            print "%s %s" % (e[0], e[1].name)
             if e[0] is None:
                 continue
 
             if self.scope != get_scope_name(e[1].name):
                 continue
+
+            self.compute_gradients_step.append(e[0])
 
             pname = get_param_name(e[1].name)
             gname = 'gradient_%s' % (pname)
@@ -150,12 +158,18 @@ class nn(object):
 
             ag = tf.summary.histogram(self.scope + '_apply_' + gname, e[1])
             cg = tf.summary.histogram(self.scope + '_compute_' + gname, e[0])
+            self.add_summary(ag)
+            self.add_summary(cg)
 
         self.apply_gradients_step = opt.apply_gradients(apply_grads, global_step=global_step)
 
-        self.sess = tf.Session()
+        config=tf.ConfigProto(
+                intra_op_parallelism_threads = 8,
+                inter_op_parallelism_threads = 8,
+            )
+        self.sess = tf.Session(config=config)
         self.summary_weights_merged = tf.summary.merge(self.summary_weights)
-        self.merged = tf.summary.merge_all()
+        self.summary_merged = tf.summary.merge(self.summary_all)
 
         init = [tf.global_variables_initializer(), tf.local_variables_initializer()]
         self.sess.run(init)
@@ -164,7 +178,9 @@ class nn(object):
     def train(self, states, qvals, want_grads=False):
         self.train_num += 1
 
-        ops = [self.merged, self.optimizer_step, self.loss]
+        #print "train scope: %s, self.scope: %s, compute_gradients_step: %s" % (tf.get_variable_scope().name, self.scope, self.compute_gradients_step)
+
+        ops = [self.summary_merged, self.summary_weights_merged, self.optimizer_step, self.loss]
         if want_grads:
             ops.append(self.compute_gradients_step)
 
@@ -174,18 +190,22 @@ class nn(object):
             })
         summary = ret[0]
         self.summary_writer.add_summary(summary, self.train_num)
+        summary_weights = ret[1]
+        self.summary_writer.add_summary(summary_weights, self.train_num)
 
-        grad = None
+        dret = {}
         if want_grads:
-            grad = ret[3]
+            grads = ret[4]
+            for k, v in zip(self.gradient_names, grads):
+                dret[k] = v
 
-        loss = ret[2]
-        return loss, grad
+        loss = ret[3]
+        return loss, dret
 
     def compute_gradients(self, states, qvals):
         self.train_num += 1
 
-        summary, grads = self.sess.run([self.merged, self.compute_gradients_step], feed_dict={
+        summary, grads = self.sess.run([self.summary_merged, self.compute_gradients_step], feed_dict={
                 self.scope + '/x:0': states,
                 self.scope + '/y:0': qvals,
             })
@@ -198,6 +218,10 @@ class nn(object):
         return dret
 
     def apply_gradients(self, grads):
+        if len(grads) == 0:
+            print "empty gradients to apply"
+            return
+
         feed_dict = {}
         #print "apply: %s" % grads
         for n, g in grads.iteritems():
@@ -205,7 +229,8 @@ class nn(object):
             #print "apply gradients to %s" % (gname)
             feed_dict[gname] = g.read()
 
-        return self.sess.run([self.apply_gradients_step], feed_dict=feed_dict)
+        summary_weights, grads = self.sess.run([self.summary_weights_merged, self.apply_gradients_step], feed_dict=feed_dict)
+        self.summary_writer.add_summary(summary_weights, self.train_num)
 
     def predict(self, states):
         p = self.sess.run([self.model], feed_dict={
@@ -214,13 +239,7 @@ class nn(object):
         return p[0]
 
     def export_params(self):
-        d = {}
-
-        with tf.variable_scope(self.scope, reuse=True):
-            for p in self.transform_params:
-                d[p] = tf.get_variable(p)
-
-        return self.sess.run(d)
+        return self.sess.run(self.transform_params)
 
     def transform(self, x1, x2):
         lr = 0.9
